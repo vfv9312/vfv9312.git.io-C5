ddf27e1c88dc2d7f29b009ebce779ffa5b04c8e6
Vladimir Farrera <vfv9312@gmail.com>
2023-03-28 23:09:15 -0600

    modifique el nav para que cambie al ser movil o tablet o escritorio usando javascript

---
+++ main.js
@@ -0,0 +1,8 @@
+const menuMovil = document.querySelector('.menu11'); // menuMovil es una constante que le damos el valor de la clase css de .menu11 que es nuestra imagen como iconito de tres lineas
+const vermenu = document.querySelector('.mobile-menu'); // vermenu es una constante con el valor del html que esta en la clase .mobile-menu
+
+menuMovil.addEventListener('click', statusinactivo); // escuchamos el evento click del ..menu1 osea del cuando le den click al icono y entramos a la funcion
+
+function statusinactivo() { // entramos a la funcion y le dieron click
+    vermenu.classList.toggle('inactivo'); //toggle lo que hace esque quita o pone la clase inactivo cada que entre la funcion por lo que el css se quita y se pone
+}
